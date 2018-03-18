14c01d517825e5ae22fcb07e7ba133187a00b6bc
TomatoMus <sy12250@gmail.com>
2018-03-18 19:06:50 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import Appbar from './src/components/Appbar';
+// import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+// import LoginScreen from './src/screens/LoginScreen';
+import SignupScreen from './src/screens/SignupScreen';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Appbar />
+        <SignupScreen />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFDF6',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 78,
+  },
+});
---
+++ src/elements/CircleButton.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Font } from 'expo';
+
+import fontAwesome from '../../assets/fonts/fa-regular-400.ttf';
+
+class CircleButton extends React.Component {
+  state = {
+    fontLoaded: false,
+  }
+
+  async componentWillMount() {
+    await Font.loadAsync({
+      FontAwesome: fontAwesome,
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
+  render() {
+    const { style, color } = this.props;
+
+    let bgColor = '#E31676';
+    let textColor = '#fff';
+
+    if (color === 'white') {
+      const tmp = bgColor;
+      bgColor = textColor;
+      textColor = tmp;
+    }
+
+    return (
+      <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
+        {
+          this.state.fontLoaded ? (
+            <Text style={[styles.circleButtonTitle, { color: textColor }]}>
+              {this.props.children}
+            </Text>
+          ) : null
+        }
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  circleButton: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  circleButtonTitle: {
+    fontFamily: 'FontAwesome',
+    fontSize: 32,
+    lineHeight: 32,
+  },
+});
+
+export default CircleButton;
