f71e1f9b477592be90d0d4ef1ddf34975c7fd5de
TomatoMus <sy12250@gmail.com>
2018-03-22 18:37:36 +0900

    Add firebase env

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 // import React from 'react';
 // import { StyleSheet } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import firebase from 'firebase';
 
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
@@ -8,15 +9,29 @@ import MemoEditScreen from './src/screens/MemoEditScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 
+import ENV from './env.json';
+
+const config = {
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.AUTH_DOMAIN,
+  databaseURL: ENV.DB_URL,
+  projectId: ENV.PROJECT_ID,
+  storageBucket: ENV.FIREBASE_STORAGE,
+  messagingSenderId: ENV.SENDER_ID,
+};
+firebase.initializeApp(config);
+
 const App = StackNavigator({
+  Login: { screen: LoginScreen },
+  Signup: { screen: SignupScreen },
   Home: { screen: MemoListScreen },
   MemoDetail: { screen: MemoDetailScreen },
   MemoEdit: { screen: MemoEditScreen },
-  Login: { screen: LoginScreen },
-  Signup: { screen: SignupScreen },
 }, {
   navigationOptions: {
     headerTitle: "memot",
+    headerTintColor: '#fff',
+    headerBackTitle: null,
     headerTitleStyle: {
       color: '#fff',
     },
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -2,15 +2,38 @@ import React from 'react';
 import { StyleSheet, View, Text, TextInput, TouchableHighlight } from 'react-native';
 
 class LoginScreen extends React.Component {
+  state = {
+    email: '',
+    pasword: '',
+  }
+
+  handleSubmit() {
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>
           ログイン
         </Text>
-        <TextInput style={styles.input} value="Email Address" />
-        <TextInput style={styles.input} value="Password" />
-        <TouchableHighlight style={styles.button} underlayColor="#C70F66" onPress={() => {}}>
+        <TextInput
+          style={styles.input}
+          value={this.state.email}
+          onChangeText={(text) => { this.setState({ email: text }); }}
+          autoCapitaliza="none"
+          autoCorrect={false}
+          placeholder="Email Address"
+        />
+        <TextInput
+          style={styles.input}
+          value={this.state.password}
+          onChangeText={(text) => { this.setState({ password: text }); }}
+          autoCapitaliza="none"
+          autoCorrect={false}
+          placeholder="Password"
+          secureTextEntry
+        />
+        <TouchableHighlight style={styles.button} underlayColor="#C70F66" onPress={this.handleSubmit.bind(this) }>
           <Text style={styles.buttonTitle}>ログインする</Text>
         </TouchableHighlight>
       </View>
