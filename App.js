e01c066c6a37ceacd70376ada1a963651537639e
TomatoMus <sy12250@gmail.com>
2018-03-23 23:10:24 +0900

    Store memos with cloud store

--- App.js
+++ App.js
@@ -6,18 +6,22 @@ import firebase from 'firebase';
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 
 import ENV from './env.json';
 
+// Required for side-effects
+require("firebase/firestore");
+
 const config = {
   apiKey: ENV.FIREBASE_API_KEY,
-  authDomain: ENV.AUTH_DOMAIN,
-  databaseURL: ENV.DB_URL,
-  projectId: ENV.PROJECT_ID,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL: ENV.FIREBASE_DB_URL,
+  projectId: ENV.FIREBASE_PROJECT_ID,
   storageBucket: ENV.FIREBASE_STORAGE,
-  messagingSenderId: ENV.SENDER_ID,
+  messagingSenderId: ENV.FIREBASE_SENDER_ID,
 };
 firebase.initializeApp(config);
 
@@ -26,6 +30,7 @@ const App = StackNavigator({
   Signup: { screen: SignupScreen },
   Home: { screen: MemoListScreen },
   MemoDetail: { screen: MemoDetailScreen },
+  MemoCreate: { screen: MemoCreateScreen },
   MemoEdit: { screen: MemoEditScreen },
 }, {
   navigationOptions: {
